5ff1e4b736f85f8eabc07f10d014e25b26dd8844
Luna Hogart <moontochka@gmail.com>
2021-12-12 17:14:57 +0500

    закончила

--- js/accordion.js
+++ js/accordion.js
@@ -1,23 +1,22 @@
 let accHeading = document.querySelectorAll(".accordion");
-let accPanel = document.querySelectorAll(".accordion-panel");
+let accPanel = document.querySelectorAll(".accordion_panel");
 
 for (let i = 0; i < accHeading.length; i++) {
-    // Execute whenever an accordion is clicked
     accHeading[i].onclick = function() {
         if (this.nextElementSibling.style.maxHeight) {
-           hidePanels();     // Hide All open Panels
+           hidePanels();
         } else {
-           showPanel(this);  // Show the panel
+           showPanel(this);
         }
     };
 }
-// Function to Show a Panel
+
 function showPanel(elem) {
   hidePanels();
   elem.classList.add("active");
   elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
 }
-// Function to Hide all shown Panels
+
 function hidePanels() {
   for (let i = 0; i < accPanel.length; i++) {
       accPanel[i].style.maxHeight = null;
